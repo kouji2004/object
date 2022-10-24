bd159c4ed2dbcac93b2c99ec24cd0ae79b74c3e1
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-24 18:41:47 +0900

    オブジェクトとは属性と操作（メソッド）

--- chord/StuSample.java
+++ chord/StuSample.java
@@ -1,29 +1,35 @@
 package chord;
 
-public class java1 {
+public class StuSample {
   public static void main(String[] ages) {
+    java stu1 = new java();
+    stu1.name = "三浦";
+    stu1.setScore(90, 90);
 
+    stu1.display();
+    System.out.println("平均" + stu1.getAvg() + "点");
   }
 }
 
-class Student {
+// 設計図
+class java {
   // メンバー変数（属性）
   String name;
   int engScore;
   int mathScore;
 
   // メソッド（操作）
-  void display() {//引数なし、戻り値なし
+  void display() {// 引数なし、戻り値なし
     System.out.println(name + "さん");
     System.out.println("英語" + engScore + "点" + mathScore + "点");
   }
 
-  void setScore(int eng, int math) {//引数あり、戻り値なし
+  void setScore(int eng, int math) {// 引数あり、戻り値なし
     engScore = eng;
     mathScore = math;
   }
 
-  double getAvg() {//引数なし、戻り値あり
+  double getAvg() {// 引数なし、戻り値あり
     double avg = (engScore + mathScore) / 2.0;
     return avg;
   }
---
+++ chord/java.java
@@ -0,0 +1,25 @@
+package chord;
+
+// 設計図
+class java {
+  // メンバー変数（属性）
+  String name;
+  int engScore;
+  int mathScore;
+
+  // メソッド（操作）
+  void display() {// 引数なし、戻り値なし
+    System.out.println(name + "さん");
+    System.out.println("英語" + engScore + "点" + mathScore + "点");
+  }
+
+  void setScore(int eng, int math) {// 引数あり、戻り値なし
+    engScore = eng;
+    mathScore = math;
+  }
+
+  double getAvg() {// 引数なし、戻り値あり
+    double avg = (engScore + mathScore) / 2.0;
+    return avg;
+  }
+}
