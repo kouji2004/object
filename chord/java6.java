bf047c9df792acd47d8c9004c92b9c9abc466674
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-25 18:29:34 +0900

    オーバーロードの演習問題

---
+++ chord/java6.java
@@ -0,0 +1,33 @@
+package chord;
+
+public class java6 {
+  public static void main(String[] ages) {
+    Car car = new Car();
+    car.setNo(2525);
+    car.run(30);
+    car.display();
+
+    Car car2 = new Car();
+    car2.setNo(123);
+    car2.display();
+  }
+}
+
+class Car {
+  // 属性
+  int no;
+  int speed;
+
+  // 操作
+  void setNo(int n) {
+    no = n;
+  }
+
+  void run(int s) {
+    speed = s;
+  }
+
+  void display() {
+    System.out.println("ナンバー" + no + "の速度は" + speed + "です");
+  }
+}
