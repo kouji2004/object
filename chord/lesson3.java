25d37d9a5a5e18d4848f3339f1a9d019939e6afb
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-25 20:33:01 +0900

    演習 private(コンパイルエラーをみつける)

---
+++ chord/lesson3.java
@@ -0,0 +1,44 @@
+package chord;
+
+public class lesson3 {
+  public static void main(String[] ages) {
+    Car c1 = new Car();
+    c1.run(30);
+    c1.display();
+
+    Car c2 = new Car(2525);
+    c2.run(50);
+    // c2.speed = 50;
+    c2.display();
+  }
+}
+
+class Car {
+  // 属性を指定
+  private int no;
+  private int speed;
+
+  public Car() {
+    no = 0;
+  }
+
+  public Car(int n) {
+    no = n;
+  }
+
+  public void run(int s) {
+    speed = s;
+  }
+
+  public void brake() {
+    speed = 0;
+  }
+
+  public void brake(int s) {
+    speed = speed - s;
+  }
+
+  public void display() {
+    System.out.println("ナンバー" + no + "の速度は" + speed + "です");
+  }
+}
